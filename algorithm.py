9afe9a43c6fd02632131702212ac50af0ea303d3
Charli <charli.vannood@gmail.com>
2025-09-18 14:35:52 +0200

    fixed compare

--- algorithm.py
+++ algorithm.py
@@ -16,13 +16,53 @@ def compare_codes(code, guess):
         if guessTried[i] == codeTried[i]:
             result[0] += 1
             codeTried[i], guessTried[i] = -1, -2
-    result[1] = len([True for pin in guessTried if pin in codeTried])
+    for i in range(len(code)):
+        print(codeTried, guessTried)
+        if guessTried[i] in codeTried:
+            result[1] += 1
+            codeTried[codeTried.index(guessTried[i])], guessTried[i] = -1, -2
     return tuple(result)
+
+def simple_strategy(colors):
+    pass
 
 
 """
 ----------------------------------------------[ HU TESTRAAMWERK ]----------------------------------------------------
 """
+
+def create_codes(base, length):
+    codes = []
+    def recursion(base, length, recur):
+        for i in range(base):   
+            tempinput = recur.copy()
+            tempinput.append(i+1)
+            if length > 1:
+                recursion(base,length-1,tempinput)
+            else:
+                codes.append(tempinput)
+    recursion(base,length,[])
+    return codes
+
+def test_compare_codes():
+    test_cases = [
+        ((1,1,1,1),(625,0,0,0,0,500,0,0,0,0,150,0,0,0,0,20,0,0,0,0,1,0,0,0,0)),
+        ((1,1,1,2),(256,308,61,0,0,317,156,27,0,0,123,24,3,0,0,20,0,0,0,0,1,0,0,0,0)),
+        ((1,1,2,2),(256,256,96,16,1,256,208,36,0,0,114,32,4,0,0,20,0,0,0,0,1,0,0,0,0)),
+        ((1,1,2,3),(81,276,222,44,2,182,230,84,4,0,105,40,5,0,0,20,0,0,0,0,1,0,0,0,0)),
+        ((1,2,3,4),(16,152,312,136,9,108,252,132,8,0,96,48,6,0,0,20,0,0,0,0,1,0,0,0,0)),
+    ]
+    codes = create_codes(6, 4)
+    for comparecode, test_result in test_cases:
+        resulttally = [0 for _ in range(25)] 
+        for code in codes:
+            result = compare_codes(code, comparecode)
+            resulttally[result[0]*(len(code)+1)+result[1]] += 1
+            # if result == (0,4) and comparecode == (1,1,1,2):
+            #     print(f"STOPOPOP: {code}, {result}")
+        assert tuple(resulttally) == test_result, (
+            f"Fout: jouw functie compare met code {comparecode} geeft \n{resulttally} als resultaten in plaats van \n{test_result}.\n"
+        )
 
 
 def test_compare():
@@ -37,10 +77,14 @@ def test_compare():
         (((1, 1, 2, 2), (2, 2, 3, 3)), (0, 2)),
         (((1, 1, 2, 2), (3, 1, 1, 3)), (1, 1)),
         (((3, 3, 4, 4), (4, 5, 3, 6)), (0, 2)),
+        (((4, 2, 3, 4), (1, 1, 2, 3)), (0, 2)),
+        (((1, 1, 1, 2), (3, 3, 2, 1)), (0, 2)),
+        (((1, 1, 1, 2), (6, 6, 2, 1)), (0, 2)),
+        (((6, 6, 2, 1), (1, 1, 1, 2)), (0, 2))
     ]
 
     for test_params, test_result in test_cases:
-        result = compare_codes(*test_params)
+        result = tuple(compare_codes(*test_params))
         assert result == test_result, (
             f"Fout: jouw functie compare{test_params} geeft {result} in plaats van {test_result}.\n"
         )
@@ -51,6 +95,7 @@ def run_tests():
         print("\x1b[0;32m", end='')
 
         test_compare()
+        # test_compare_codes()
         print("Je functie compare() doorstaat de tests!")
 
         print("\x1b[0;0m", end='')
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from algorithm import run_tests
+from algorithm import run_tests, simple_strategy
 
 run_tests()
+
+guess = simple_strategy([1, 2, 3, 4, 5, 6])
